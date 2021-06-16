128cec0116d881762cc7333a8d5432ec972bdc20
“Jakob <steiningerjakob@gmail.com>
2021-06-16 16:41:03 +0200

    Remove ESLint ignore file (next.config.js)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,6 +19,7 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
